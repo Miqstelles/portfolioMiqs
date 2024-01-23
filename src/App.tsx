8b976d7b878b101689c35bbeb9748eae99f619f3
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2024-01-23 20:43:32 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="flex flex-col justify-center items-center w-screen h-screen bg-gradient-to-r from-purple-700 via-purple-400 to-purple-700">
+      <div className="flex flex-col justify-center m-[20%]">
+        <h2 className="text-[1.25rem] sm:text-[2rem] ml-[5px] lg:ml-[18px] text-left text-white font-extrabold leading-none">
+          OLÁ, ME CHAMO MIQUÉIAS
+        </h2>
+        <h1 className="text-[3.5rem] sm:text-[6rem] lg:text-[12rem] text-justify text-white font-extrabold leading-none">
+          SOFTWARE DEVELOPER
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
