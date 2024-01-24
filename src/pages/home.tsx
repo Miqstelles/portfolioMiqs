cdac1b2f7ca2046c1cb90ad56645a996c584e617
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2024-01-24 10:52:59 -0300

    feat: add home page

---
+++ src/pages/home.tsx
@@ -0,0 +1,13 @@
+import { Stack } from "../components/stack";
+import { Info } from "../components/info";
+
+export function Home() {
+  return (
+    <div className="flex flex-col justify-center items-center w-full h-[940px] bg-gradient-to-r from-purple-700 via-purple-400 to-purple-700">
+      <div className="flex flex-col justify-center m-[20%]">
+        <Info />
+        <Stack />
+      </div>
+    </div>
+  );
+}
