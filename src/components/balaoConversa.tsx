ef55f715ac3d1e1badafbcde54f698fa750a8a31
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2024-01-26 13:52:30 -0300

    refactor:: avoid repeating code in stack componente

--- src/components/balaoConversa.tsx
+++ src/components/balaoConversa.tsx
@@ -5,7 +5,7 @@ interface BalaoInfo {
 
 export function BalaoConversa(props: BalaoInfo) {
   return (
-    <div className="mt-[74px] z-30 absolute  transition-all duration-200 balaoConversa">
+    <div className="mt-[74px] z-30 absolute transition-all duration-200 balaoConversa">
       <div
         className={`${props.corFundo} size-[20px] m-auto rotate-45 rounded-e-lg mb-[-12px] relative`}
       ></div>
--- src/components/stack.tsx
+++ src/components/stack.tsx
@@ -1,92 +1,63 @@
 import {
-  SiGit,
+  SiTypescript,
   SiJavascript,
-  SiNodedotjs,
   SiReact,
   SiTailwindcss,
-  SiTypescript,
+  SiNodedotjs,
+  SiGit,
 } from "react-icons/si";
+
 import { BalaoConversa } from "./balaoConversa";
+import { IconType } from "react-icons";
+
+interface StackInfo {
+  IconComponent: IconType;
+  title: string;
+  color: string;
+}
+
+const stacks: StackInfo[] = [
+  { IconComponent: SiTypescript, title: "Typescript", color: "#358EF1" },
+  { IconComponent: SiJavascript, title: "Javascript", color: "#F1DD35" },
+  { IconComponent: SiReact, title: "React", color: "#04D8F9" },
+  { IconComponent: SiTailwindcss, title: "TailwindCSS", color: "#3490D0" },
+  { IconComponent: SiNodedotjs, title: "Node.js", color: "#68A063" },
+  { IconComponent: SiGit, title: "GIT", color: "#df654c" },
+];
 
 export function Stack() {
   const css = `
-  .balaoConversa {
-    opacity: 0;
-    transition: opacity 300ms, transform 300ms; 
-    transform: scale(1.05);
-  }
-  
-  .stack:hover + .balaoConversa {
-    display: block;
-    opacity: 1;
-    transform: scale(1); 
-  }
-  
+    .balaoConversa {
+      opacity: 0;
+      transition: opacity 300ms, transform 300ms; 
+      transform: scale(1.05);
+    }
+    
+    .stack:hover + .balaoConversa {
+      display: block;
+      opacity: 1;
+      transform: scale(1); 
+    }
   `;
-
   return (
-    <div className="flex gap-8 ml-[12px]">
+    <div className="flex gap-8 ml-[14px]">
       <style>{css}</style>
 
-      <div className="flex flex-col items-center">
-        <SiTypescript
-          className="hover:fill-[#247EE6] transition-all duration-200 drop-shadow-lg hover:scale-105 stack"
-          size={64}
-          color="#358EF1"
-        />
-        <BalaoConversa titulo="Typescript" corFundo="bg-[#358EF1]" />
-      </div>
+      {stacks.map((stack, index) => (
+        <div key={index} className="flex flex-col items-center">
+          <stack.IconComponent
+            className={`hover:fill-[${stack.color}]transition-all duration-200 drop-shadow-lg stack`}
+            size={64}
+            color={stack.color}
+            id="stack"
+          />
 
-      <div className="flex flex-col items-center">
-        <SiJavascript
-          className="hover:fill-[#D3C015] transition-all duration-200 drop-shadow-lg stack"
-          size={64}
-          color="#F1DD35"
-        />
-        <BalaoConversa titulo="Javascript" corFundo="bg-[#F1DD35]" />
-      </div>
-
-      <div className="flex flex-col items-center">
-        <SiReact
-          className="hover:fill-[#06B5Cf] transition-all duration-200 drop-shadow-lg stack"
-          size={64}
-          color="#04D8F9"
-        />
-        <BalaoConversa titulo="React" corFundo="bg-[#04D8F9]" />
-      </div>
-
-      <div className="flex flex-col items-center">
-        <SiTailwindcss
-          className="hover:fill-[#2383c7] transition-all duration-200 drop-shadow-lg stack"
-          size={64}
-          color="#3490D0"
-          title="Tailwind CSS"
-          id="stack"
-        />
-        <BalaoConversa titulo="TailwindCSS" corFundo="bg-[#3490D0]" />
-      </div>
-
-      <div className="flex flex-col items-center">
-        <SiNodedotjs
-          className="hover:fill-[#3C873A] transition-all duration-200 drop-shadow-lg stack"
-          size={64}
-          color="#68A063"
-          title="Node.js"
-          id="stack"
-        />
-        <BalaoConversa titulo="Node.js" corFundo="bg-[#68A063]" />
-      </div>
-
-      <div className="flex flex-col items-center">
-        <SiGit
-          className="hover:fill-[#b84129] transition-all duration-200 drop-shadow-lg stack"
-          size={64}
-          color="#df654c"
-          title="GIT"
-          id="stack"
-        />
-        <BalaoConversa titulo="GIT" corFundo="bg-[#df654c]" />
-      </div>
+          <BalaoConversa
+            titulo={stack.title}
+            corFundo={`bg-[${stack.color}]`}
+          />
+        </div>
+      ))}
     </div>
   );
 }
